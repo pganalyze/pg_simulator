32792d55cc5e518eda6d95152e9718838daa6ecc
Lukas Fittl <lukas@fittl.com>
2014-07-13 20:51:10 +0200

    Clean up a bit.

--- lib/pg_simulator/environment.rb
+++ lib/pg_simulator/environment.rb
@@ -3,21 +3,23 @@ require 'securerandom'
 module PgSimulator
   class Environment
     # Note: Supplied connection needs to have CREATEDB rights
-    def initialize(otherconn, schema)
+    def initialize(otherconn, schema_hsh)
       @dbname = "pg_simulator_%s" % SecureRandom.uuid.gsub('-', '_')
       @otherdb = {dbname: otherconn.db, host: otherconn.host, port: otherconn.port, user: otherconn.user}
       
       otherconn.exec_params("CREATE DATABASE %s" % otherconn.quote_ident(@dbname))
       @conn = PG.connect(@otherdb.merge(dbname: @dbname))
       
-      create_schema!(schema)
+      create_schema!(schema_hsh)
     rescue => e
       destroy if @conn
       raise
     end
     
-    def create_schema!(schema)
-      schema.each do |table|
+    # Schema hash needs to be structured like the one sent by
+    # pganalyze-collector
+    def create_schema!(schema_hsh)
+      schema_hsh.each do |table|
         colstats = {}
         columns = table['columns']
         columns.sort_by! {|c| c['position'] }
@@ -40,11 +42,6 @@ module PgSimulator
                                      FROM pg_catalog.pg_class
                                     WHERE relnamespace = (SELECT oid FROM pg_catalog.pg_namespace WHERE nspname = $1)
                                           AND relname = $2", [table['schema_name'], table['table_name']])[0]['oid']
-        
-        # relpages has to match the physical pages the planner can see (otherwise it scales reltuples)
-        #table['stats']['relpages'] = 0
-        # FIXME: Actually this is sub-optimal too. The planner still sees the zero pages, but has some heuristics to assume its not a seqscan. This skews the behaviour. We should try to get the planner to ignore the actual physical pages.
-        # See http://www.postgresql.org/docs/9.3/static/row-estimation-examples.html
         
         @conn.exec_params "UPDATE pg_catalog.pg_class
                               SET relpages = $1, reltuples = $2, relallvisible = $3
